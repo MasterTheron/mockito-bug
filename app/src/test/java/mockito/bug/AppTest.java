79d6399d7074d55f1dbf1794bcc9e6f016ac65b3
Christian Conrad <christian.conrad@schwarm.com>
2023-01-11 16:12:10 +0100

    Auto Fomat not working

--- app/src/test/java/mockito/bug/AppTest.java
+++ app/src/test/java/mockito/bug/AppTest.java
@@ -7,9 +7,16 @@ import org.junit.Test;
 
 public class AppTest {
     @Test
-    public void testAppHasAGreeting() {
+    public void testFormatNotWorking() {
         when(true).thenReturn(false);
+            App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test
+    public void testFormatWorking() {
         App classUnderTest = new App();
+        when(true).thenReturn(false);
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
